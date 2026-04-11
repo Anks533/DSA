b27a3b3e2a1f38ccde7d39ccd0d2ecdb2c55af4f
Anks533 <34959914+Anks533@users.noreply.github.com>
2026-04-11 01:12:41 -0400

    15. 3Sum

---
+++ src/main/java/com/leetcode/twopointers/array/threeSum/Solution15.java
@@ -0,0 +1,54 @@
+package com.leetcode.twopointers.array.threeSum;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution15 {
+
+    public static void main(String[] args) {
+        List<List<Integer>> result = threeSum(new int[] {-4,-2,1,-5,-4,-4,4,-2,0,4,0,-2,3,1,-5,0});
+        System.out.print(result);
+    }
+
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> threeSumList = new ArrayList<>();
+        Arrays.sort(nums);
+        for(int i=0; i< nums.length-2;i++) {
+            int target = -nums[i]; // first number i.e target of 2 sum - [a+b+c = 0 or b+c = -a]
+            if( i > 0 && nums[i] == nums[i-1]) {
+               continue;
+            }
+
+            int j = i+1;
+            int k = nums.length-1;
+
+            while(j<k) {
+                int sum = nums[j] + nums[k];
+                if(sum > target) {
+                    k--;
+                } else if (sum < target) {
+                    j++;
+                } else {
+                    // equal
+                    List<Integer> tuples = new ArrayList<>();
+                    tuples.add(-target);
+                    tuples.add(nums[j]);
+                    tuples.add(nums[k]);
+                    threeSumList.add(tuples);
+                    j++;
+                    k--;
+
+                    while(j < nums.length &&  nums[j] == nums[j-1]) {
+                        j++;
+                    }
+
+                    while(k >= 0 &&  nums[k] == nums[k+1]) {
+                        k--;
+                    }
+                }
+            }
+        }
+        return  threeSumList;
+    }
+}
